38bd69165e739f45deb63a3e76414fa027baec76
RahmaMandoh <rahmamandoh12@gmail.com>
2025-01-29 21:06:54 +0200

    ToDo list at couse TypeScript DONE!

--- 6-TypeScript/Day1/ToDoList/build/ToDo.js
+++ 6-TypeScript/Day1/ToDoList/build/ToDo.js
@@ -1,26 +1,36 @@
 var add = document.getElementById("Plus");
 var input = document.getElementById("text");
-var inputFields = document.getElementById("InputFilds"); // فكرتك كانت هنا أنك تضيفي المحتوى في هذا العنصر
-add.addEventListener("click", function (e) {
-    if (input.value) {
-        var htmlContent = "\n        <div class=\"child\">\n            <div>\n                <span class=\"\">".concat(input.value, "</span>\n            </div>\n\n            <div>\n                <img id=\"Delete\" src=\"svgexport-15 (10).svg\"/>\n                <img id=\"Edit\" src=\"Edit.svg\"/>\n            </div>\n        </div>");
+var inputFields = document.getElementById("InputFilds");
+add.addEventListener("click", function () {
+    if (input.value.trim()) {
+        var child_1 = document.createElement("div");
+        child_1.classList.add("child");
+        child_1.innerHTML = "\n            <div>\n                <span class=\"text-content\">".concat(input.value, "</span>\n                <input type=\"text\" class=\"edit-input\" style=\"display: none;\" />\n            </div>\n            <div>\n                <img class=\"delete-btn\" src=\"svgexport-15 (10).svg\" alt=\"Delete\"/>\n                <img class=\"edit-btn\" src=\"Edit.svg\" alt=\"Edit\"/>\n                <button class=\"save-btn\" style=\"display: none;\">Save</button>\n            </div>\n        ");
         if (inputFields) {
-            inputFields.innerHTML += htmlContent;
-            input.value = "";
-            var deleteButtons = document.querySelectorAll("#Delete");
-            deleteButtons.forEach(function (button) {
-                button.addEventListener("click", function () {
-                    var _a;
-                    (_a = button.closest(".child")) === null || _a === void 0 ? void 0 : _a.remove();
-                });
-            });
-            var editButton = document.querySelectorAll("#Edit");
-            editButton.forEach(function (button) {
-                button.addEventListener("click", function () {
-                    button.addEventListener("click", function () {
-                    });
-                });
-            });
+            inputFields.appendChild(child_1);
         }
+        input.value = "";
+        var deleteButton = child_1.querySelector(".delete-btn");
+        var editButton = child_1.querySelector(".edit-btn");
+        var saveButton_1 = child_1.querySelector(".save-btn");
+        var textSpan_1 = child_1.querySelector(".text-content");
+        var editInput_1 = child_1.querySelector(".edit-input");
+        deleteButton.addEventListener("click", function () {
+            child_1.remove();
+            console.log("Deleted!");
+        });
+        editButton.addEventListener("click", function () {
+            editInput_1.value = textSpan_1.innerText;
+            textSpan_1.style.display = "none";
+            editInput_1.style.display = "inline-block";
+            saveButton_1.style.display = "inline-block";
+            editInput_1.focus();
+        });
+        saveButton_1.addEventListener("click", function () {
+            textSpan_1.innerText = editInput_1.value;
+            textSpan_1.style.display = "inline";
+            editInput_1.style.display = "none";
+            saveButton_1.style.display = "none";
+        });
     }
 });
--- 6-TypeScript/Day1/ToDoList/src/ToDo.ts
+++ 6-TypeScript/Day1/ToDoList/src/ToDo.ts
@@ -1,39 +1,54 @@
-var add = document.getElementById("Plus");
+var add = document.getElementById("Plus") as HTMLButtonElement;
 var input = document.getElementById("text") as HTMLInputElement;
-var inputFields = document.getElementById("InputFilds"); // فكرتك كانت هنا أنك تضيفي المحتوى في هذا العنصر
+var inputFields = document.getElementById("InputFilds");
 
-add.addEventListener("click", function(e) {
-    if (input.value) {
-        let htmlContent: string = `
-        <div class="child">
-            <div>
-                <span class="">${input.value}</span>
-            </div>
+add.addEventListener("click", function () {
+    if (input.value.trim()) {
+        let child = document.createElement("div");
+        child.classList.add("child");
 
+        child.innerHTML = `
             <div>
-                <img id="Delete" src="svgexport-15 (10).svg"/>
-                <img id="Edit" src="Edit.svg"/>
+                <span class="text-content">${input.value}</span>
+                <input type="text" class="edit-input" style="display: none;" />
             </div>
-        </div>`;
-        
+            <div>
+                <img class="delete-btn" src="svgexport-15 (10).svg" alt="Delete"/>
+                <img class="edit-btn" src="Edit.svg" alt="Edit"/>
+                <button class="save-btn" style="display: none;">Save</button>
+            </div>
+        `;
+
         if (inputFields) {
-            inputFields.innerHTML += htmlContent; 
-            input.value = "";
-
-            var deleteButtons = document.querySelectorAll("#Delete");
-            deleteButtons.forEach(button => {
-                button.addEventListener("click", function() {
-                button.closest(".child")?.remove();
-                });
-            });
-            var editButton = document.querySelectorAll("#Edit");
-            editButton.forEach(button =>{
-                button.addEventListener("click", function(){
-                    button.addEventListener("click", function() {
-                        
-                    });
-                });
-            });
+            inputFields.appendChild(child);
         }
+
+        input.value = "";
+
+        let deleteButton = child.querySelector(".delete-btn") as HTMLImageElement;
+        let editButton = child.querySelector(".edit-btn") as HTMLImageElement;
+        let saveButton = child.querySelector(".save-btn") as HTMLButtonElement;
+        let textSpan = child.querySelector(".text-content") as HTMLSpanElement;
+        let editInput = child.querySelector(".edit-input") as HTMLInputElement;
+
+        deleteButton.addEventListener("click", function () {
+            child.remove();
+            console.log("Deleted!");
+        });
+
+        editButton.addEventListener("click", function () {
+            editInput.value = textSpan.innerText; 
+            textSpan.style.display = "none"; 
+            editInput.style.display = "inline-block"; 
+            saveButton.style.display = "inline-block"; 
+            editInput.focus();
+        });
+
+        saveButton.addEventListener("click", function () {
+            textSpan.innerText = editInput.value; 
+            textSpan.style.display = "inline"; 
+            editInput.style.display = "none"; 
+            saveButton.style.display = "none"; 
+        });
     }
 });
